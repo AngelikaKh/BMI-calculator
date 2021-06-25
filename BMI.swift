11b3456423374643bbac0117c1f18a8ab1ab6ca6
Angelika <54324618+AngelikaKh@users.noreply.github.com>
2021-06-25 15:19:38 +0400

    Add files via upload

---
+++ BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Angelika Khodzhaian
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
